a8a0d856d5557630ac90c9c5b89a9b906c9e9a66
oliverzi <oliver.zivcic@gmail.com>
2022-07-16 01:10:15 +0200

    Add project files.

---
+++ since-birth/since-birth.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <string>
+
+int main()
+{
+    int d, m, y;
+    char date[20];
+
+    printf_s("Enter date of birth (dd.mm.yyyy.): ");
+    scanf_s("%20s",&date,20);
+
+
+    return 0;
+}
